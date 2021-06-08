8ba49606bc098995b6c12c8ccaab355cf53e2be9
Ale Bausa <alebausa@macbook-pro-de-ale-1.home>
2021-06-08 11:17:14 +0200

    adds product photos

---
+++ sick-fits/backend/keystone.ts
@@ -0,0 +1,61 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 130,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User', // Schema responsible for logging in
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add initial roles here
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: add data seeding here
+    },
+    lists: createSchema({
+      // Schema items go in here
+      Product,
+      ProductImage,
+      User,
+    }),
+    ui: {
+      // To-do: change this for roles
+      isAccessAllowed: ({ session }) => {
+        console.log(session);
+        return !!session?.data;
+      },
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL query
+      User: 'id name',
+    }),
+  })
+);
