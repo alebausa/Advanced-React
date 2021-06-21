2b143f798260e2e15310c1a5a240632765fb850b
Ale Bausa <alebausa@macbook-pro-de-ale-1.home>
2021-06-21 14:55:49 +0200

    adds Create Product component

--- sick-fits/frontend/components/CreateProduct.js
+++ sick-fits/frontend/components/CreateProduct.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';
+import useForm from '../lib/useForm';
 
 export default function CreateProduct() {
-  const [name, setName] = useState('Product');
+  const { inputs, handleChange, clearForm, resetForm } = useForm({
+    name: 'Nice shoes',
+    price: 2595,
+    description: 'These are the best shoes',
+  });
   return (
     <form>
       <label htmlFor="name">
@@ -11,10 +15,38 @@ export default function CreateProduct() {
           id="name"
           name="name"
           placeholder="Name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          value={inputs.name}
+          onChange={handleChange}
         />
       </label>
+      <label htmlFor="price">
+        Price
+        <input
+          type="number"
+          id="price"
+          name="price"
+          placeholder="Price"
+          value={inputs.price}
+          onChange={handleChange}
+        />
+      </label>
+      <label htmlFor="description">
+        Description
+        <input
+          type="text"
+          id="description"
+          name="description"
+          placeholder="Description"
+          value={inputs.description}
+          onChange={handleChange}
+        />
+      </label>
+      <button type="button" onClick={clearForm}>
+        Clear form
+      </button>
+      <button type="button" onClick={resetForm}>
+        Reset form
+      </button>
     </form>
   );
 }
